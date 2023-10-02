7cb461129a9eee47ab6d3bb201a2a1757f20e7dd
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-10-02 13:30:38 +0700

    Create Main.php

---
+++ src/NurAzliYT/GameChat/Main.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace NurAzliYT\GameChat;
+
+use DaPigGuy\libPiggyEconomy\exceptions\MissingProviderDependencyException;
+use DaPigGuy\libPiggyEconomy\exceptions\UnknownProviderException;
+use DaPigGuy\libPiggyEconomy\libPiggyEconomy;
+use DaPigGuy\libPiggyEconomy\providers\EconomyProvider;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener {
+
+    private int $reward;
+    public ?string $word = null;
+    public array $words = [];
+    public static $instance;
+
+    /** @var EconomyProvider */
+    private $economyProvider;
+
+    /**
+     * @throws MissingProviderDependencyException
+     * @throws UnknownProviderException
+     */
+    protected function onEnable(): void {
+        self::$instance = $this;
+
+        libPiggyEconomy::init();
+        $this->economyProvider = libPiggyEconomy::getProvider($this->getConfig()->get("economy"));
+
+        $this->loadWords();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getScheduler()->scheduleDelayedTask(new WordTask(), (20 * 60 * $this->getConfig()->get("Scramble-Time")));
+    }
+
+    public function getEconomyProvider(): EconomyProvider {
+        return $this->economyProvider;
+    }
+
+    public function onChat(playerChatEvent $event) {
+        $player = $event->getPlayer();
+        $msg = $event->getMessage();
+
+        if ($msg !== null && $this->word !== null && strtolower($msg) == strtolower($this->word)) {
+            $event->cancel();
+            $this->rewardPlayer($player);
+            $this->loadWords();
+            $this->word = null;
+        }
+    }
+
+
+    public function loadWords() {
+        foreach ($this->getConfig()->get("Words") as $word) {
+            $this->words[] = $word;
+        }
+    }
+
+    public function rewardPlayer($player) {
+        $this->getServer()->broadcastMessage("§6" . $player->getName() . " Guessed The Word Correctly.\n§6The Word Was §e" . $this->word);
+        $this->getEconomyProvider()->giveMoney($player, (int) $this->reward);
+    }
+
+    public function scrambleWord() {
+        $onlinePlayers = $this->getServer()->getOnlinePlayers();
+
+        if (count($onlinePlayers) > $this->getConfig()->get("Online-Players")) {
+            $this->word = $this->words[array_rand($this->words)];
+            $this->reward = mt_rand($this->getConfig()->get("Min-Reward"), $this->getConfig()->get("Max-Reward"));
+            foreach ($this->getServer()->getOnlinePlayers() as $player) {
+                $player->sendMessage("§b Unscramble The Word §e" . str_shuffle($this->word) . " §bReceive $" . $this->reward . "!");
+            }
+            $this->getScheduler()->scheduleDelayedTask(new WordTask(), (20 * 60 * $this->getConfig()->get("Scramble-Time")));
+        }
+    }
+
+    public static function getInstance(): Main {
+        return self::$instance;
+    }
+}
